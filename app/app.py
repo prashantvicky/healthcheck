b1eb39a7e6cd3653e3a141fe7c7ea33047ffb8f2
Matt Seashore <mseashor@cisco.com>
2018-12-06 21:26:54 -0800

    first

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import subprocess
+import shlex
+import json
+import sys
+import time
+import os
+from time import sleep
+from texttable import Texttable
+
+
+NORMAL="\033[0m"
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+PEACHBLOW = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
